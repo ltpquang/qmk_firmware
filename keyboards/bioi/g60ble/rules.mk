96b896a67b1acfc86e8362de93a4eff9ab99ac3c
Quang Le <phuc.quang102@gmail.com>
2022-09-14 23:44:21 +0700

    BLE comfig for G60BLE

--- keyboards/bioi/g60ble/rules.mk
+++ keyboards/bioi/g60ble/rules.mk
@@ -7,6 +7,14 @@ F_CPU = 8000000
 # Bootloader selection
 BOOTLOADER = qmk-dfu
 
+SRC += usart.c \
+       ble.c \
+       main.c
+
+OPT_DEFS += -DPROTOCOL_BLE
+OPT_DEFS += -DUART_RX1_BUFFER_SIZE=16 -DUART_TX1_BUFFER_SIZE=16
+OPT_DEFS += -DUSART1_ENABLED
+
 # Build Options
 #   change yes to no to disable
 #
